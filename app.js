99c672499036af06dad512cdce0ba56b41724ee2
shahimayank10 <shahimayank10@gmail.com>
2025-04-25 12:17:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const path=require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
